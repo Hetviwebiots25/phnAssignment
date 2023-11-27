c4064192a62956c96dedff0ce4a0651452619b0d
Hetvi Dalal <82036039+Hetviwebiots25@users.noreply.github.com>
2023-11-27 09:51:18 +0530

    Update email.blade.php

--- enquiry-form-data/resources/views/enquiry/email.blade.php
+++ enquiry-form-data/resources/views/enquiry/email.blade.php
@@ -3,12 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Email</title>
+    <title>Test Enquiry Email</title>
 </head>
 <body>
     <h1>Hello!</h1>
-    <p>This is a test email sent from your Laravel application.</p>
-    <p>Feel free to customize this email template based on your project's needs.</p>
-    <p>Thank you!</p>
+   <p>This is form sending email.
+    <p>Thank you! for filling enquiry form. </p>
 </body>
 </html>
